6c613afe739fa954a5a21ee1cccc06f93c725a44
Michaël Hauspie <michael.hauspie@univ-lille1.fr>
2016-03-10 11:49:56 +0100

    Fix build configuration
    Uses gcc to link so that path are automatically detected correctly

--- dbuild.config.mk
+++ dbuild.config.mk
@@ -1,7 +1,9 @@
 
-CFLAGS += -march=armv6z -g -Wall -Wextra
+CFLAGS += -march=armv6z -g -Wall -Wextra -ffreestanding
 CFLAGS += -I $(BASE)FreeRTOS/Source/portable/GCC/RaspberryPi/
 CFLAGS += -I $(BASE)FreeRTOS/Source/include/
 CFLAGS += -I $(BASE)Demo/Drivers/
+
+LDFLAGS += -lc -nostdlib
 
 TOOLCHAIN=arm-none-eabi-
